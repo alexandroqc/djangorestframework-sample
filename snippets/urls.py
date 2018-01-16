5a9481af1be67f459b9be1e8f4ac5885a2549455
Alejandro Quisbert Carvajal <alexandro@autistici.org>
2018-01-15 23:36:44 -0400

    Requests y Responses

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, re_path
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
     path('snippets/', views.snippet_list),
     re_path('snippets/(?P<pk>[0-9]+)/', views.snippet_detail),
 ]
+
+## Format suffixes: http://www.django-rest-framework.org/api-guide/format-suffixes/
+urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json', 'html', 'api'])
