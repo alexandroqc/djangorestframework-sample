dd2d420853a8074cbc74a6fbbd5838fb35af2314
Alejandro Quisbert Carvajal <alexandro@autistici.org>
2018-01-19 13:47:13 -0400

    Relaciones, Hyperlinked APIs

--- snippets/urls.py
+++ snippets/urls.py
@@ -3,10 +3,23 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
-    path('snippets/', views.SnippetList.as_view()),
-    re_path('snippets/(?P<pk>[0-9]+)/', views.SnippetDetail.as_view()),
-    path('users/', views.UserList.as_view()),
-    re_path('snippets/(?P<pk>[0-9]+)/', views.UserDetail.as_view()),
+    path('',
+        views.api_root),
+    path('snippets/',
+        views.SnippetList.as_view(),
+        name='snippet-list'),
+    re_path('snippets/(?P<pk>[0-9]+)/',
+        views.SnippetDetail.as_view(),
+        name='snippet-detail'),
+    re_path('snippets/(?P<pk>[0-9]+)/highlight/',
+        views.SnippetHighlight.as_view(),
+        name='snippet-highlight'),
+    path('users/',
+        views.UserList.as_view(),
+        name='user-list'),
+    re_path('users/(?P<pk>[0-9]+)/',
+        views.UserDetail.as_view(),
+        name='user-detail'),
     path('api-auth/', include('rest_framework.urls')),
 ]
 
